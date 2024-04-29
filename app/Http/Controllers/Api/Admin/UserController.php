13dc2ea86e8671c218e895f2e049ab02e039eab7
zakaria-elkoh <zakaria.developer00@gmail.com>
2024-04-29 21:38:44 +0100

    added some changes on the chat

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -142,11 +142,6 @@ class UserController extends Controller
 
 
 
-
-
-    // ==================================================================================
-
-
     public function getChattingPartners(Request $request)
     {
 
